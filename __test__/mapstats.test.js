7dd8b93f73a33d735b27552f5f384af2be7bb615
Ashutosh Kumar <kancha@lotgaming.xyz>
2023-07-20 08:50:15 +0530

    Update mapstats.test.js

--- __test__/mapstats.test.js
+++ __test__/mapstats.test.js
@@ -34,7 +34,7 @@ describe("Test the mapstats routes", () => {
     let teamData = [{
       match_id: 3,
       map_number: 1,
-      map_name: 'de_dust2',
+      map_name: 'de_anubis',
       start_time: new Date().toISOString().slice(0, 19).replace("T", " ")
     }];
     return request
