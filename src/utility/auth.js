cbbd8494bbc17a47f29e48f94bc5bb0246371ad4
Ashutosh Kumar <kancha@lotgaming.xyz>
2023-07-22 21:04:41 +0530

    ss

--- src/utility/auth.js
+++ src/utility/auth.js
@@ -229,7 +229,7 @@ passport.use('local-register',
         defaultMaps.push(['de_mirage', 'Mirage', curUser[0].id]);
         defaultMaps.push(['de_nuke', 'Nuke', curUser[0].id]);
         defaultMaps.push(['de_overpass', 'Overpass', curUser[0].id]);
-        defaultMaps.push(['de_dust2', 'Dust II', curUser[0].id]);
+        defaultMaps.push(['de_anubis', 'Anubis', curUser[0].id]);
         defaultMaps.push(['de_vertigo', 'Vertigo', curUser[0].id]);
         sql = "INSERT INTO map_list (map_name, map_display_name, user_id) VALUES ?";
         await db.query(sql, [defaultMaps]);
