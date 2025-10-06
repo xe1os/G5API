2526c371c52be1a8caa17bbe7c6a5be4ac1174b3
xe1os <39219862+xe1os@users.noreply.github.com>
2025-10-06 17:45:22 +0530

    Update auth.ts

--- src/utility/auth.ts
+++ src/utility/auth.ts
@@ -88,9 +88,9 @@ async function returnStrategy(identifier: any, profile: any, done: any) {
         defaultMaps.push(['de_ancient', 'Ancient', curUser[0].id]);
         defaultMaps.push(['de_mirage', 'Mirage', curUser[0].id]);
         defaultMaps.push(['de_nuke', 'Nuke', curUser[0].id]);
-        defaultMaps.push(['de_anubis', 'Anubis', curUser[0].id]);
+        defaultMaps.push(['de_train', 'Train', curUser[0].id]);
         defaultMaps.push(['de_dust2', 'Dust II', curUser[0].id]);
-        defaultMaps.push(['de_vertigo', 'Vertigo', curUser[0].id]);
+        defaultMaps.push(['de_overpass', 'Overpass', curUser[0].id]);
         sql = "INSERT INTO map_list (map_name, map_display_name, user_id) VALUES ?";
         await db.query(sql, [defaultMaps]);
       } else {
